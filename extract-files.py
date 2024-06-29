46b60ede68ba77f3ce7042bb62c8b8fa76a6d351
Aaron Kling <webgeek1234@gmail.com>
2024-06-28 22:09:34 -0500

    sm8650-common: Remove software omx codec references
    These are no longer available as of Android 14 QPR3. The C2 codecs
    available by default in the swcodec apex will be used instead.
    
    Change-Id: I8cdf3e700823779a7382c66c3df8410d41209b7c

--- extract-files.py
+++ extract-files.py
@@ -63,6 +63,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/media_codecs_pineapple.xml': blob_fixup()
+        .regex_replace('.*media_codecs_(google_audio|google_telephony).*\n', ''),
     'vendor/lib64/libcamximageformatutils.so': blob_fixup()
         .remove_needed('android.hardware.graphics.allocator-V1-ndk.so'),
     (
