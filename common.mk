3e679bceb65d497eaca19f1b4c7f55af54d822c4
Michael Bestas <mkbestas@lineageos.org>
2024-07-08 08:56:25 +0300

    sm8650-common: Remove media_codecs_google_c2*
    AOSP codecs can be used by default and it is not necessary to include
    them device side.
    
    Change-Id: I8227b06f0466b614f12fa9008402b92461ea3dd0

--- common.mk
+++ common.mk
@@ -164,12 +164,6 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,livedisplay_sysfs,enable_se,true)
 
-# Media
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
-
 # Memtrack
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
--- extract-files.py
+++ extract-files.py
@@ -64,7 +64,7 @@ lib_fixups: lib_fixups_user_type = {
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/etc/media_codecs_pineapple.xml': blob_fixup()
-        .regex_replace('.*media_codecs_(google_audio|google_telephony).*\n', ''),
+        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony).*\n', ''),
     'vendor/lib64/libcamximageformatutils.so': blob_fixup()
         .remove_needed('android.hardware.graphics.allocator-V1-ndk.so'),
     (
