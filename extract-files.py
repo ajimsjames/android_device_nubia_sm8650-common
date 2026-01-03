0fbcfacef74b7be96a940b93aaaebe1fbe0107bb
Igor121 <chernokalsky89@gmail.com>
2026-01-03 15:42:27 +0500

    sm8650-common: use versioned protobuf on vendor.qti.hardware.qccsyshal@1.2-halimpl.so
    Change-Id: I2c81d6917b10b83b941ba4c0622011400746a978

--- extract-files.py
+++ extract-files.py
@@ -136,6 +136,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
+    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
